d195499fafc2c165c6a69efcebcfc06c438d55c9
LStrike <schuerim@googlemail.com>
2016-01-08 21:56:00 +0100

    comments

--- binaryCount/binaryCount.ino
+++ binaryCount/binaryCount.ino
@@ -1,12 +1,10 @@
 /*
-  Blink
-  Turns on an LED on for one second, then off for one second, repeatedly.
+  binary count
+  counts in abinary way with five leds.
  
   This example code is in the public domain.
  */
  
-// Pin 13 has an LED connected on most Arduino boards.
-// give it a name:
 int led1 = 13;
 int led2 = 12;
 int led3 = 11;
@@ -15,9 +13,7 @@ int led5 = 9;
 int pause = 1000;
 int pauseShort = 50;
 
-// the setup routine runs once when you press reset:
 void setup() {                
-  // initialize the digital pin as an output.
   pinMode(led1, OUTPUT);
   pinMode(led2, OUTPUT);
   pinMode(led3, OUTPUT);
--- blink2/blink2.ino
+++ blink2/blink2.ino
@@ -1,12 +1,10 @@
 /*
-  Blink
-  Turns on an LED on for one second, then off for one second, repeatedly.
+  running light
+  Turns on an 5 LEDs, one after another, on for one second, then off for one second, repeatedly.
  
   This example code is in the public domain.
  */
  
-// Pin 13 has an LED connected on most Arduino boards.
-// give it a name:
 int led1 = 13;
 int led2 = 12;
 int led3 = 11;
@@ -14,9 +12,7 @@ int led4 = 10;
 int led5 = 9;
 int pause = 50;
 
-// the setup routine runs once when you press reset:
 void setup() {                
-  // initialize the digital pin as an output.
   pinMode(led1, OUTPUT);
   pinMode(led2, OUTPUT);
   pinMode(led3, OUTPUT);
@@ -24,14 +20,12 @@ void setup() {
   pinMode(led5, OUTPUT);
 }
 
-// the loop routine runs over and over again forever:
 void loop() {
-  digitalWrite(led1, HIGH);   // turn the LED on (HIGH is the voltage level)
+  digitalWrite(led1, HIGH);   
   digitalWrite(led2, LOW);
   digitalWrite(led3, LOW);
   digitalWrite(led4, LOW);
   digitalWrite(led5, LOW);
-  delay(pause);  // wait for a second
   digitalWrite(led2, HIGH);
   delay(pause);
   digitalWrite(led1, LOW);
